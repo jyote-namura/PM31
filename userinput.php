38a72da1679398507ec9250d981be8673a2d565f
こたに <shbsk0508@yahoo.co,jp>
2015-02-19 17:19:09 +0900

    aaaa

---
+++ userinput.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="css/common.css" />
+<link rel="stylesheet" type="text/css" href="css/userinput.css" />
+<title>このあとどうする？</title>
+
+</head>
+<body>
+
+<?php
+    include 'head.php';
+?>
+<div id="wrap">
+
+<div class="login_title">
+<img src="img/userinput.jpg">
+</div>
+
+<form action="index.php">
+	<div class="txtbox1">E-Mail<input type="text" style="width : 160px; height : 18px;"></div>
+	<div class="txtbox2">パスワード<input type="text" style="width : 160px; height : 18px;"></div>
+	<div class="txtbox3">名前<input type="text" style="width : 160px; height : 18px;"></div>
+	<div class="txtbox4"><input type="image" src="img/userinput.jpg"></div>
+</form>
+	
+</div><!--wrap-->
+<?php
+    include 'foot.php';
+?>
+</body>
