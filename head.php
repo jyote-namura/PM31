f621add78594fd01f1d1fe38846967eacc3a552d
こたに <shbsk0508@yahoo.co,jp>
2015-02-19 17:06:02 +0900

    今日の分です

--- head.php
+++ head.php
@@ -1,10 +1,10 @@
 <div id="header">
 	<div class="logo"><a href="index.php"><img src="img/logo.jpg"></a></div>
-	<div class="login"><img src="img/registration.jpg"></div>
+	<div class="toroku"><a href="userinput.php"><img src="img/registration.jpg"></a></div>
 </div><!--header-->
 <ul>
 	<li><a href="mypage.php"><img src="img/menu1.jpg"></a></li>
 	<li onclick="push()"><a href="#"><img src="img/menu2.jpg"></a></li>
 	<li><a href="#"><img src="img/menu3.jpg"></a></li>
-	<li><a href="#"><img src="img/menu4.jpg"></a></li>
+	<li><a href="login.php"><img src="img/menu4.jpg"></a></li>
 </ul>
---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="css/common.css" />
+<link rel="stylesheet" type="text/css" href="css/login.css" />
+<title>このあとどうする？</title>
+
+</head>
+<body>
+
+<?php
+    include 'head.php';
+?>
+<div id="wrap">
+
+<div class="login_title">
+<img src="img/login.jpg">
+</div>
+
+<form action="index.php">
+	<div class="txtbox1">E-Mail<input type="text" style="width : 160px; height : 18px;"></div>
+	<div class="txtbox2">パスワード<input type="text" style="width : 160px; height : 18px;"></div>
+	<div class="txtbox3"><input type="image" src="img/login.jpg"></div>
+</form>
+	
+</div><!--wrap-->
+<?php
+    include 'foot.php';
+?>
+</body>
